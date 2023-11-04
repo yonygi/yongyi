d13b602effaa2819bc6fba6db23444f9454f7c0e
yong yi <131849681+yonygi@users.noreply.github.com>
2023-11-04 22:19:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+/*== typing animation ==*/
+var typed = new Typed(".typing", {
+    strings: ["", "Digital Designer", "Web Developer", "Coder"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
